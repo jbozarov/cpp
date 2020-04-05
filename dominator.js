dfe912217ce10754d3eac1e7cfd884eb865a8648
John Bozarov <53405754+JBozarov@users.noreply.github.com>
2020-04-04 20:47:29 -0400

    Create dominator.js

---
+++ dominator.js
@@ -0,0 +1,23 @@
+A zero-indexed array arr consisting of n integers is given. The dominator of array arr is the value 
+that occurs in more than half of the elements of arr.
+For example, consider array arr such that arr = [3,4,3,2,3,1,3,3]
+The dominator of arr is 3 because it occurs in 5 out of 8 elements of arr and 5 is more than a half of 8.
+Write a function dominator(arr) that, given a zero-indexed array arr consisting of n integers, returns 
+the dominator of arr. The function should return −1 if array does not have a dominator. All values in arr will be >=0.
+
+
+
+
+function dominator(arr){
+   let count = {}; 
+   let result; 
+   arr.forEach(i => count[i] = (count[i] || 0) + 1); 
+   for ( let prop in count ){
+      if(count[prop]>arr.length/2){
+         result = prop;
+         break; 
+      }
+   }
+   if (result) return Number(result); 
+   return -1;    
+}
