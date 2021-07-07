0027d9b0c8df5b2540214a9adebabbfebec63126
jbozarov <tech@jbozarov.com>
2021-07-06 22:40:38 -0400

    Pointer to memory address

---
+++ Pointer.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std; 
+
+int main() {
+  string food = "Pizza";  // A string variable
+  string* ptr = &food;  // A pointer variable that stores the address of food
+
+  // Output the value of food
+  cout << food + "\n";
+
+  // Output the memory address of food
+  cout << &food << endl;
+
+  // Output the memory address of food with the pointer
+  cout << ptr << endl;
+  return 0;
+}
