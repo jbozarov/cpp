5ef23ac635138c3d1e1f02c36c69e6fd7f10184b
jbozarov <tech@jbozarov.com>
2021-07-06 21:42:33 -0400

    Hello CPP

---
+++ HelloWorld.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std; 
+
+int main() {
+    cout << "Hello World" << endl; 
+    cout << "It is the second line" << endl; 
+    cout << max(5, 10);
+    return 0; 
+}
