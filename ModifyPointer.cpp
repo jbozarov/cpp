f343856281186f37969d01f1639e30b575d31713
jbozarov <tech@jbozarov.com>
2021-07-06 22:49:09 -0400

    Changing pointer changes actual value

---
+++ ModifyPointer.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+  string food = "Pizza";
+  string* ptr = &food;
+
+  cout << food << "\n"; // Pizza
+
+  cout << &food << "\n"; // memory address
+
+  cout << *ptr << "\n"; // Pizza
+  
+  *ptr = "Hamburger";
+  
+  cout << *ptr << "\n"; // Hamburger
+  
+  cout << food << "\n"; // Hamburger
+
+  return 0;
+}
